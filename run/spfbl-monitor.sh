f25557cf4d3395c4fbcb7277e338180fac54d461
rbaldasso <rodrigonbaldasso@gmail.com>
2016-08-10 13:00:51 -0300

    Update spfbl-monitor.sh

--- run/spfbl-monitor.sh
+++ run/spfbl-monitor.sh
@@ -10,15 +10,44 @@ PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 #ps auxw | grep apache2 | grep -v grep > /dev/null
 #lsof -i :80 | grep httpd | grep -v grep > /dev/null
 #curl -L http://loophost.com.br
-verifica=$(curl -s -o /dev/null -w "%{http_code}" http://www.loophost.com.br)
 
 verificaWeb(){ 
 
-    curl -s -o /dev/null -w "%{http_code}" $URLSV:$IPHTTP
+    hostname=$(cat /etc/spfbl/spfbl.conf | grep hostname= | cut -d'=' -f 2)
+    porta=$(cat /etc/spfbl/spfbl.conf | grep http_port | cut -d'=' -f 2)
+    verifica=$(curl -s -o /dev/null -w "%{http_code}" $hostname:$porta)
+    if [ $verifica != 200 ]
+    then
+        reiniciaServico
+    fi
 }
 
-if [ $verifica != 301 ]
-then
-        service httpd restart > /dev/null
-        service nginx restart > /dev/null
-fi
+verificaRespostas(){ 
+
+    consulta=$(spfbl check 127.0.0.1 root localhost.localdomain)
+    
+    if [ $consulta != LAN ]
+    then
+        reiniciaServico
+    fi
+    elif [ $consulta == LAN ]
+    then
+        consulta2=$(spfbl query "200.160.7.130" "gter-bounces@eng.registro.br" "eng.registro.br" "destinatario@destino.com.br")
+        sleep 2
+        if [ $consulta2 != PASS ]
+        then
+            reiniciaServico
+        fi
+    fi
+    
+}
+
+reiniciaServico(){
+        /etc/init.d/spfbl restart > /dev/null
+        sleep 10
+    
+        if [ "$(ps auxwf | grep java | grep SPFBL | grep -v grep | wc -l)" -eq "1" ]; then
+            kill $(ps aux | grep SPFBL | grep java | grep -v grep | awk '{print $2}')
+        fi   
+}
+
