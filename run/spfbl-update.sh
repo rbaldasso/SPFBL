f93573bb6059a7531874b32dbad6cde80860b156
ADMF <admf@users.noreply.github.com>
2016-11-06 05:12:06 -0200

    Update spfbl-update.sh

--- run/spfbl-update.sh
+++ run/spfbl-update.sh
@@ -3,10 +3,15 @@
 V='\033[01;31m'
 D='\033[01;32m'
 R='\033[0m'
-echo -e "${D}SPFBL Updater v0.1 release 01/11/2016 ${R}"
+
+echo "\n${V}SPFBL Updater v0.1 release 06/11/2016 ${R}"
 
 BACKUP_DIR=/opt/spfbl/backup
+TMP_DIR=/tmp/spfbl-update
 AGORA=`date +%y-%m-%d-%H-%M`
+URLBETA=https://github.com/SPFBL/beta-test/raw/master/SPFBL.jar
+URLSTABLE=https://github.com/SPFBL/stable/raw/master/SPFBL.jar
+URLMASTER=https://github.com/SPFBL/stable/archive/master.zip
 
 if [ -f /etc/lsb-release ]; then
     . /etc/lsb-release
@@ -14,59 +19,43 @@ if [ -f /etc/lsb-release ]; then
     . /etc/init.d/functions
 fi 
 
+if [ ! -d "$TMP_DIR" ]; then
+    mkdir -p "$TMP_DIR"
+    else
+    rm -Rf "$TMP_DIR"/*
+fi
+
 atualizaSistema(){
 
-        echo -e "${V}\nIniciando procedimentos${R}"
-        echo -e "${D}\n${R}"
-        mkdir -p /tmp/spfbl-update
-        rm -Rf /tmp/spfbl-update/*
         if [ ! -d "$BACKUP_DIR" ]; then
             mkdir -p "$BACKUP_DIR"
         fi
-        #Baixa Arquivos
-        wget "$URLDOWNLOAD" -O /tmp/spfbl-update/SPFBL.jar &> /dev/null
+
+        #Compara Arquivos
         var1=$(stat -c%s /tmp/spfbl-update/SPFBL.jar)
         var2=$(stat -c%s /opt/spfbl/SPFBL.jar)
 
     if [ "$var1" != "$var2" ]; then
         #Necessario atualizar
-        echo -e "${D}Os arquivos são diferentes, o SPFBL será atualizado${R}"
-        echo -e "${V}!!!!!!! Para cancelar, tecle CTRL+C AGORA! !!!!!!!!${R}"
-        echo
-        sleep 10
-        echo -e "${D}Continuando atualização...${R}"
+        echo "${D}\n Os arquivos são diferentes, o SPFBL será atualizado${R}"
         fazBackup
-        echo
-        echo -e "${D}Verificando LIBs necessárias...${R}"
-        cd /tmp/spfbl-update/
-        wget https://github.com/leonamp/SPFBL/archive/master.zip &> /dev/null
-        unzip master.zip &> /dev/null
-        cd /tmp/spfbl-update/SPFBL-master/lib
-        find . -name "*jar" -type f -exec ls -l {} \; | cut -d'/' -f 2 >> /tmp/spfbl-update/listadelibsGITHUB.txt
-        cd /opt/spfbl/lib
-        find . -name "*jar" -type f -exec ls -l {} \; | cut -d'/' -f 2 >> /tmp/spfbl-update/listadelibsLOCAL.txt
-            if diff -q /tmp/spfbl-update/listadelibsGITHUB.txt /tmp/spfbl-update/listadelibsLOCAL.txt
-                then
-                echo "LIBs iguais, nao necessario altera-las"
-            else
-                echo "LIBs diferentes, necessario update"
-                echo -e "${D}\nAtualizando Libs ${R}"
-            fi
-        rm -f /opt/spfbl/lib/*
-        cp /tmp/spfbl-update/SPFBL-master/lib/* /opt/spfbl/lib/
+        
+        echo "${D}\n Verificando LIBs necessárias...${R}"
+        cd "$TMP_DIR"
+        wget "$URLMASTER" -O /tmp/spfbl-update/master.zip > /dev/null
+        unzip master.zip > /dev/null
+        cp -n /tmp/spfbl-update/stable-master/lib/* /opt/spfbl/lib
 
         #Atualiza SPFBL.jar
         echo "Movendo o arquivo SPFBL.jar"
         echo
             if [ ! -f "/tmp/spfbl-update/SPFBL.jar" ]; then
-                echo "Can't download https://github.com/SPFBL/beta-test/raw/master/SPFBL.jar"
+                echo "Can't download URL/file, please check initial config."
                 exit
             fi 
 
         echo "****   Current Version   ****"
         mostraVersao
-
-        paraMta
 
         echo "**** SPFBL - Shutdown    ****"
         echo "SHUTDOWN" | nc 127.0.0.1 9875
@@ -77,17 +66,15 @@ atualizaSistema(){
         echo "OK"
 
         echo "**** SPFBL - Starting    ****"
-        /etc/init.d/spfbl start
+        iniciaSPFBL
         sleep 30
 
         if [ "$(ps auxwf | grep java | grep SPFBL | grep -v grep | wc -l)" -eq "1" ]; then
             echo "OK - SERVICE ONLINE"
         else
-            echo "FALHA - Verifique os logs e se necessario reverta do backup"
+            echo "FALHA - Verifique os logs e se necessario reverta o backup"
         fi
 
-        iniciaMta
-         
         echo "**** SPFBL - New Version ****"
         mostraVersao
 
@@ -96,40 +83,46 @@ atualizaSistema(){
         
     else
         
-        echo "Os arquivos são iguais"
+        echo "Os arquivos são iguais, nenhum procedimento foi efetuado."
 
     fi
 
 } 
 
+iniciaSPFBL(){
+
+    if [ -f /etc/init.d/spfbl ]; then
+    /etc/init.d/spfbl start
+    else
+    cd /opt/spfbl
+    java -jar SPFBL.jar &
+    fi
+}
+
 iniciaMta(){
 
-    if [ "$MTA" != "nenhum" ]; then
         echo "**** !  Starting MTA   ! ****"
-        service "$MTA" start
+        service "$2" start
         echo "OK"
-    fi
 
 }
 
 paraMta(){
 
-    if [ "$MTA" != "nenhum" ]; then
         echo "**** !!  Stoping MTA  !! ****"
-        service "$MTA" stop
+        service "$2" stop
         echo "OK"
-    fi
 
 }
 
 fazBackup(){
 
-    echo -e "${V} **** SPFBL - Store cache ****${R}" 
+    echo "${V} **** SPFBL - Store cache ****${R}" 
     echo "STORE" | nc 127.0.0.1 9875
 
-    echo -e "${V} **** SPFBL - Backup      ****${R}"
+    echo "${V} **** SPFBL - Backup      ****${R}"
     echo "DUMP" | nc 127.0.01 9875 > "$BACKUP_DIR"/spfbl-dump-"$AGORA".txt
-    tar -zcf "$BACKUP_DIR"/spfbl-backup-"$AGORA".tar /opt/spfbl &> /dev/null
+    tar -zcf "$BACKUP_DIR"/spfbl-backup-"$AGORA".tar /opt/spfbl --exclude "$BACKUP_DIR" &> /dev/null
     echo "BACKUP OK"
 
 }
@@ -140,43 +133,60 @@ mostraVersao(){
 
 }
 
+downloadBeta(){
+
+    cd /tmp/spfbl-update/
+    wget "$URLBETA" -O /tmp/spfbl-update/SPFBL.jar > /dev/null
+
+}
+
+downloadStable(){
+
+    cd /tmp/spfbl-update/
+    wget "$URLSTABLE" -O /tmp/spfbl-update/SPFBL.jar > /dev/null
+
+}
+
+
 ############
 
-if [ "$1" != "-u" ] && [ "$2" != "--m" ]; then
-    echo "${V}\nScript de atualização do SPFBL${R}"
+mostraHelp(){
     echo "${D}\nParametros necessarios: ${R}"
-    echo "${D}\n   -u  stable ou beta ${R}" 
+    echo "${D}\n Selecione o canal: stable ou beta ${R}" 
     echo "${D}   stable: versao estavel (opcao recomendada) ${R}" 
     echo "${D}   beta: versao de teste ${R}"
-    echo "${D}\n   -m  nenhum ou exim ou postix ou o nome do servico MTA${R}" 
+    echo "${D}\n Selecione o serviço MTA: nenhum ou exim ou postix ${R}" 
     echo "${D}\n Exemplo: ${R}"
-    echo "${D}\n ./spfbl-update.sh -u stable -m nenhum ${R}"
-    echo
+    echo "${D}\n ./spfbl-update.sh stable nenhum \n ${R}"
     exit
-fi
+}
 
-# Opcoes de controle (menu)
-while [[ $# -gt 1 ]]
-do
-key="$1"
-
-case $key in
-    -m|--mta)
-    MTA="$2"
-    shift # past argument
-    ;;
-    -u|--update)
-    BRANCH="$2"
-    shift # past argument
-    ;;
+case $2 in
+  'exim'|'postfix')
+    paraMTA;;
+  nenhum)
+     echo "Nenhum MTA selecionado.";;
+  *)
+    mostraHelp
+    exit;;
 esac
-shift # past argument or value
-done
 
-if [ "$BRANCH" == "beta" ]; then
-    URLDOWNLOAD="https://github.com/SPFBL/beta-test/raw/master/SPFBL.jar"
-else 
-    URLDOWNLOAD="https://github.com/leonamp/SPFBL/blob/master/dist/SPFBL.jar"
-fi
+
+case $1 in
+  beta)
+    downloadBeta;;
+  stable)
+    downloadStable;;
+  *)
+    mostraHelp
+    exit;;
+esac
 
 atualizaSistema
+
+case $2 in
+  'exim'|'postfix')
+    iniciaMTA;;
+  *)
+    exit;;
+esac
